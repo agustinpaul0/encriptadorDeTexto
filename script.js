137b34d48dba66c55b5ffb6172f746c4674c374f
Agustin <a.agustinpaul@gmail.com>
2022-09-03 20:50:58 -0300

    Restrigiendo el uso de mayúsculas y caracteres especiales

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const munhecoText = document.querySelector(".texto1-munheco");
 const txtCifrado = document.querySelector(".texto2-munheco");
 const informacion = document.querySelector(".informacion");
 
+function Numeros(string){//Solo minúsculas y sin acentos ni caracteres especiales
+
+    var out = '';
+    var filtro = 'abcdefghijklmnñopqrstuvwxyz1234567890';//Caracteres validos
+	
+    //Recorrer el texto y verificar si el caracter se encuentra en la lista de validos 
+    for (var i=0; i<string.length; i++)
+       if (filtro.indexOf(string.charAt(i)) != -1) 
+             //Se añaden a la salida los caracteres validos
+	     out += string.charAt(i);
+	
+    //Retornar valor filtrado
+    return out;
+    
+} 
+
 function btnEncriptar() {
 
 	const textoEncriptado = encriptar(inputTexto.value);
